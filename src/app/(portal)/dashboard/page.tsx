883e5616369921fadf00121f9bbd9328e3567630
Tlangelani Mboweni <mbowenit@gmail.com>
2025-05-02 16:17:35 +0200

    add prisma

--- src/app/(portal)/dashboard/page.tsx
+++ src/app/(portal)/dashboard/page.tsx
@@ -1,12 +1,16 @@
 import ExpensesForm from '@/components/expenses-form';
 import ExpensesList from '@/components/expenses-list';
+import { prisma } from '@/lib/db';
+import { Expense } from '@/types';
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const expenses: Expense[] = await prisma.expense.findMany();
+
   return (
     <main>
       <h1 className="text-3xl font-bold text-white text-center mt-2">Dashboard</h1>
       <div className="w-full max-w-[600px] mx-auto">
-        <ExpensesList />
+        <ExpensesList expenses={expenses} />
         <ExpensesForm />
       </div>
     </main>
--- src/components/expenses-list.tsx
+++ src/components/expenses-list.tsx
@@ -1,22 +1,16 @@
-const ExpensesList = () => {
-  const expenses = [
-    {
-      id: 1,
-      description: 'Car Insurance',
-      amount: 2200,
-    },
-    {
-      id: 2,
-      description: 'Groceries',
-      amount: 4500,
-    },
-  ];
+import { Expense } from '@/types';
+
+interface Props {
+  expenses: Expense[];
+}
+
+const ExpensesList = async ({ expenses }: Props) => {
   return (
     <ul className="h-[300px] bg-white rounded mt-4 shadow-md">
       {expenses.map((expense) => (
         <li key={expense.id} className="flex items-center px-4 py-2 border-b">
           <p>{expense.description}</p>
-          <p className="ml-auto font-bold mr-[15px]">{expense.amount}</p>
+          <p className="ml-auto font-semibold mr-[15px]">R{expense.amount}</p>
           <button className="text-[10px] h-[20px] w-[20px] bg-red-500 text-white rounded-full hover:bg-red-600">
             X
           </button>
